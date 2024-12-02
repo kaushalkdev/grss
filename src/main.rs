e99c2d46d843c4810a583234015d792d232cd972
Kaushal <kaushalk.dev@gmail.com>
2024-12-02 13:28:12 +0530

    added clap to manage cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser)]
 struct Cli {
+    /// The pattern to look for.
     pattern: String,
+    /// The path to the file to read.
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("No args given");
-    let path: String = std::env::args().nth(2).expect("No path given");
-
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
-
-    print!("pattern {:?} {:?}", args.pattern, args.path);
+    let args = Cli::parse();
+    println!("pattern: {:?}  {:?}", args.pattern, args.path);
 }
