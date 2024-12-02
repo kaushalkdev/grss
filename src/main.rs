a8fc985e7b62e88f958a39037c2388f92a20d386
Kaushal <kaushalk.dev@gmail.com>
2024-12-02 11:54:53 +0530

    added clap dependency, added a struct for data management

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pattern = std::env::args().nth(1).expect("No args given");
+    let path: String = std::env::args().nth(2).expect("No path given");
+
+    let args = Cli {
+        pattern,
+        path: std::path::PathBuf::from(path),
+    };
+
+    print!("pattern {:?} {:?}", args.pattern, args.path);
 }
