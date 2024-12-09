812fba29815543524e9e84be0e48d462dd615c63
Kaushal <kaushalk.dev@gmail.com>
2024-12-09 21:42:10 +0530

    handled error with anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -8,14 +9,14 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path)
-        .expect("\n ===========> INCORRECT FILE FORMAT <==========\n");
-
-    for line in content.lines() {
+        .with_context(|| format!("Could not read file `{}`", args.path.display()));
+    for line in content?.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+    Ok(())
 }
